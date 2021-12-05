a4168c203ccd1292fe4cd757c3b564194b3ad412
olie304 <olie304@users.noreply.github.com>
2021-12-05 16:04:38 -0600

    added emissive objective

--- decompiled/sorter.py
+++ decompiled/sorter.py
@@ -15,16 +15,3 @@ for root, dirs, files in os.walk("."):
             #print(root + '\\' + filename + "   -->   " + '.\\' + '\\'.join(name_path[:name_end]) + '\\' + filename)
             Path('.\\' + '\\'.join(name_path[:name_end])).mkdir(parents=True, exist_ok=True)
             os.rename(root + '\\' + filename, '.\\' + '\\'.join(name_path[:name_end]) + '\\' + filename)
-            #category = filename.split('_')[0]
-            #Path("./" + category).mkdir(parents=True, exist_ok=True)
-            #os.rename(filename, f'./{category}/{filename}')"""
-            #print(root + "\\" +  file)
-
-"""for filename in os.listdir():
-    if '_' in filename and (filename.endswith('.hlsl') or filename.endswith('.fx') or filename.endswith('.asm')):
-        name_path = filename.split('_')
-        name_end = (len(name_path) - 1 - name_path[::-1].index('main'))
-        print(os.getcwd() + '/'.join(name_path[:name_end]))
-        #category = filename.split('_')[0]
-        #Path("./" + category).mkdir(parents=True, exist_ok=True)
-        #os.rename(filename, f'./{category}/{filename}')"""
